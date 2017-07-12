146475b94e109f4705e1644e9f9c46fc4db318f1
Nicholas Lee <nicholasle89@hotmail.com>
2017-07-12 16:33:43 -0700

    server class: server init
    Signed-off-by: Nicholas Lee <nicholasle89@hotmail.com>

--- server/server.py
+++ server/server.py
@@ -1,9 +1,16 @@
 import socket
-
+import sys
 
 class Server:
 
-    def __int__(self):
+    def __int__(self, hostname, port, idcounter, freeid, sendQ, receiveQ, CMDController):
+        self.hostname = hostname
+        self.port = port
+        self.idcounter = idcounter
+        self.freeid = freeid
+        self.sendQ = sendQ
+        self.receiveQ = receiveQ
+        self.CMDController = CMDController
 
     def process_incoming_con(self):
 
