62b49f354e8ab163ebc58f270371ef38f37914f9
Lucas Girouard-Stranks <lustranks@gmail.com>
2017-07-14 11:26:15 -0700

    augment queue test

--- shared/concurrent_queue.py
+++ shared/concurrent_queue.py
@@ -39,19 +39,32 @@ class TestConcurrentQueue():
     def is_empty(self):
         queue = ConcurrentQueue()
         assert queue.isEmpty() is True
-        queue.push("Hello")
-        assert queue.isEmpty() is False
+
+        for x in range(0, 20):
+            queue.push("Hello" + str(x))
+            assert queue.isEmpty() is False
+
+        for x in range(0, 19):
+            queue.pop()
+            assert queue.isEmpty() is False
+
         queue.pop()
         assert queue.isEmpty() is True
 
     def size(self):
         queue = ConcurrentQueue()
         assert queue.size() is 0
-        queue.push("Hello")
-        assert queue.size() is 1
+
+        for x in range(0, 20):
+            queue.push("Hello" + str(x))
+            assert queue.size() is x + 1
+
+        for x in range(0, 19):
+            queue.pop()
+            assert queue.size() is 20 - 1 - x
+
         queue.pop()
         assert queue.size() is 0
-
 
 if __name__ == '__main__':
     test = TestConcurrentQueue()
