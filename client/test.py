21719d4c51b2684f6ddfe3d403a1a629e0ee7a78
tristanp2 <tristanp2@hotmail.com>
2017-07-17 18:18:56 -0700

    Fixed path grabbing code

--- client/test.py
+++ client/test.py
@@ -1,22 +1,28 @@
 import sys
-path = sys.argv[0].split('\\')
+import os
+#path = sys.argv[0].split('\\')
+path = os.path.abspath(__file__)
+path = path.split('\\')
 path = path[:-2]
 path = '\\'.join(path)
 path = path + '\\shared'
-print(path)
 sys.path.append(path)
-print(sys.path)
 import ui
 from time import sleep
 import curses
 from message import Message
 
+
 if __name__ == '__main__':
     # do tests
     test_ui = ui.UI()
     msg = Message()
+    elapsed = 0
+    delta = 30
     while True:
-        if test_ui.temp_login_done:
+        if elapsed > 1000:
+            elapsed = 0
             test_ui.process_message(msg)
-        sleep(1)
-    print('here')
+        test_ui.update_chat()
+        elapsed += delta
+        curses.napms(delta)
