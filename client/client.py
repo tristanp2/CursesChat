0b334ace2d00cf811e8d2d495beb89c87228d5a9
tristanp2 <tristanp2@hotmail.com>
2017-07-17 19:19:21 -0700

    Started working on client class
    - path appending is now done at start of client class
    - UI is now controlled through client main loop

--- client/client.py
+++ client/client.py
@@ -1,32 +1,52 @@
-import socket
-from .messenger import Messenger
-from .message_handler import MessageHandler
+import os
+import sys
+path = os.path.abspath(__file__)
+path = path.split('\\')
+path = path[:-2]
+path = '\\'.join(path)
+sys.path.append(path)
 
-# from enum import Enum
-# from datetime import datetime.time
+
+import socket
+from shared.concurrent_queue import ConcurrentQueue
+from ui import UI
+from threading import Thread
+from messenger import Messenger
+from message_handler import MessageHandler
+from time import sleep
 
 class Client:
-
     def __init__(self):
         self.socket = socket.socket()
         self.messenger = Messenger(self.socket)
         self.message_handler = MessageHandler(self.socket)
-
+        
+        self.receive_queue = ConcurrentQueue()
+        self.ui = UI()
         #TODO: Initialize threads.
 
-        #TODO: start main loop thread
         #TODO: start io loop thread
 
     def main_loop(self):
+        self.alias = self.ui.do_login()
+        self.ui.start_chat()
+        while True:
+            while not self.receive_queue.isEmpty():
+                msg = self.receive_queue.pop()
+                self.ui.process_message(msg)
+            outgoing = self.ui.get_outgoing()
+            for m in outgoing:
+                m.set_alias(self.alias)
+                self.receive_queue.push(m)
+            self.ui.update_chat()
+            sleep(0.5)
 
-        x = 1
-        while True:
-            print("Main: To infinity and beyond! " + str(x))
-            x += 1
-            
+    #send and recieve       
     def io_loop(self):
-        
         x = 1
         while True:
-            print("IO: To infinity and beyond! " + str(x))
             x += 1
+
+if __name__ == '__main__':
+    cl = Client()
+    cl.main_loop()
