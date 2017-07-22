c713834aa0630585b4c4402a72a3ced22327bc13
tristanp2 <tristanp2@hotmail.com>
2017-07-22 16:38:51 -0700

    Added exception handling to client

--- client/client.py
+++ client/client.py
@@ -2,8 +2,6 @@ import socket
 import queue
 from ui import UI
 from threading import Thread
-from message_receiver import MessageReceiver
-from message_sender import MessageSender
 from time import sleep
 
 class Client:
@@ -11,8 +9,6 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #asumming the server is localhost and port is 10000
         self.server_adrs = ('134.87.170.182', 10000)
-        self.receiver = MessageReceiver(self.socket)
-        self.sender = MessageSender(self.socket)
         self.received_queue = queue.Queue()
         self.outgoing_queue = queue.Queue()
         self.ui = UI()
@@ -22,34 +18,46 @@ class Client:
 
     def main_loop(self):
         self.alias = self.ui.start_login()
-        self.socket.connect(self.server_adrs)
-        self.ui.end_login()
-        self.send_thread.start()
-        self.recv_thread.start()
-        self.ui.start_chat()
-        while True:
-            try:
-                while True:
-                    msg = self.received_queue.get_nowait()
-                    self.ui.parse_and_push(msg)
-            except queue.Empty:
-                pass
-            outgoing = self.ui.get_outgoing()
-            for m in outgoing:
-                m.set_alias(self.alias)
-                self.outgoing_queue.put(m)
-            self.ui.update_chat()
-            sleep(0.2)
+        try:
+            self.socket.connect(self.server_adrs)
+            self.ui.end_login()
+            self.send_thread.start()
+            self.recv_thread.start()
+            self.ui.start_chat()
+            while True:
+                try:
+                    while True:
+                        msg = self.received_queue.get_nowait()
+                        self.ui.parse_and_push(msg)
+                except queue.Empty:
+                    pass
+                outgoing = self.ui.get_outgoing()
+                for m in outgoing:
+                    m.set_alias(self.alias)
+                    self.outgoing_queue.put(m)
+                self.ui.update_chat()
+                sleep(0.2)
+        except OSError:
+            #exit here
+            pass
     
     def __send_loop(self):
-        while True:
-            msg = self.outgoing_queue.get()
-            self.socket.send(msg.to_string().encode())
+        try:
+            while True:
+                msg = self.outgoing_queue.get()
+                self.socket.send(msg.to_string().encode())
+        except OSError:
+            #exit here
+            pass
 
     def __recv_loop(self):
-        while True:
-            data = self.socket.recv(1024)
-            self.received_queue.put(data.decode())
+        try:
+            while True:
+                data = self.socket.recv(1024)
+                self.received_queue.put(data.decode())
+        except OSError:
+            #exit here
+            pass
 
 if __name__ == '__main__':
     cl = Client()
