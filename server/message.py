f5b9e487068438f92f7bcf48a0d843318cd72784
Nicholas Lee <nicholasle89@hotmail.com>
2017-07-24 20:39:43 -0700

    working on CMDcontroller
    Signed-off-by: Nicholas Lee <nicholasle89@hotmail.com>

--- server/message.py
+++ server/message.py
@@ -6,8 +6,8 @@ class Message:
     #TODO: add cid
     #note: it does not handle type for now
     #TODO: will take away alias eventually
-    def __init__(self, alias, type, payload):
-        #self.clientID = clientID
+    def __init__(self ,cid ,alias, type, payload):
+        self.cid = cid
         self.alias = alias
         self.type = MessageType(int(type))
         self.payload = ' '.join(payload)
