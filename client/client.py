490917be5740c3a2585015cf37afffec4233aa0f
tristanp2 <tristanp2@hotmail.com>
2017-07-25 17:32:16 -0700

    Some reorganization of client side

--- client/client.py
+++ client/client.py
@@ -6,12 +6,13 @@ import threading
 from message_type_client import MessageType
 from message_client import Message
 from time import sleep
+from datetime import datetime
 
 class Client:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #asumming the server is localhost and port is 10000
-        self.server_adrs = ('134.87.133.12', 10000)
+        self.server_adrs = ('134.87.170.182', 10000)
         self.received_queue = queue.Queue()
         self.outgoing_queue = queue.Queue()
         self.ui = UI()
@@ -24,21 +25,38 @@ class Client:
         self.recv_thread = threading.Thread(None, self.__recv_loop, 'recv_t')
         self.recv_thread.setDaemon(True)
 
+    def parse_received(self, string):
+        args = string.split(' ')
+        msg = None
+        if len(args) >= 2:
+            alias = args[0]
+            mtype = MessageType(int(args[1]))
+            time = datetime.strptime(args[2], Message.time_format)
+            payload = ' '.join(args[3:])
+            msg = Message(mtype, payload, time, alias)
+        return msg
+
     def main_loop(self):
         self.alias = self.ui.start_login(self.server_adrs[0])
         self.login_message.set_alias(self.alias)
         try:
             self.socket.connect(self.server_adrs)
+            self.recv_thread.start()
+            self.send_thread.start()
             self.socket.send(self.login_message.to_string().encode())
             self.ui.end_login()
-            self.send_thread.start()
-            self.recv_thread.start()
+            
             self.ui.start_chat()
             while True and not self.exit:
                 try:
                     while True:
-                        msg = self.received_queue.get_nowait()
-                        self.ui.parse_and_push(msg)
+                        string = self.received_queue.get_nowait()
+                        msg = self.parse_received(string)
+                        type = msg.get_type()
+                        if type == MessageType.alias:
+                            self.alias = msg.get_payload()
+                        else:
+                            self.ui.push_received(msg)
                 except queue.Empty:
                     pass
                 outgoing = self.ui.get_outgoing()
@@ -51,7 +69,7 @@ class Client:
                 self.ui.update_chat()
                 sleep(0.2)
         except OSError:
-            self.__set_exit('Exception in main loop')
+            self.__set_exit('Networking exception')
         self.ui.do_exit(self.exit_msg)
 
     def __set_exit(self, msg = None):
